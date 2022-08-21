4e596ee0acf41e65c230ce82183f747dff5277d6
Sajjad <pb.sajjad@gmail.com>
2022-08-21 21:01:39 +0430

    Build project

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,7 @@
-const path = require("path");
-
 module.exports = {
   i18n: {
     locales: ["default", "fa", "en"],
     defaultLocale: "default",
     localeDetection: false,
-    localePath: path.resolve("./public/locales"),
   },
 };
